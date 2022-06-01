3f0ee0c425ff0ad164584a7ff273865eacbb76b1
AlexRvaFlutter <Alex.Rva.Flutter.com>
2022-06-01 19:05:29 +0300

    setup project

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_application/screen/home_screen/home_screen.dart';
+import 'package:ui_application/screens/home_screen/home_screen.dart';
 import 'package:ui_application/setup.dart';
 
 class App extends StatelessWidget {
